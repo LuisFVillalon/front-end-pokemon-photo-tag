cface3e226dc1b3570c2ff427e3e60ad85dd708f
Luis F Villalon <104875261+LuisFernandoVillalon@users.noreply.github.com>
2025-04-10 22:21:33 -0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"], // include all React files
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
