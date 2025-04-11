72c843fb80f64b464d8bb46b79d10dd8618f203d
Luis F Villalon <104875261+LuisFernandoVillalon@users.noreply.github.com>
2025-04-10 22:21:09 -0700

    Made Home page mobile friendly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import '../style.css';
-import React from 'react';
 import Levels from '../components/Levels';
 import ScoreBoard from '../components/Scoreboard';
 
 function Home(props) {
   return (
     <div>
-      <header>Where&apos;s that Pokemon?</header>
+      <div class="header-container">
+        <header>Where&apos;s that Pokemon?</header>
+      </div>
       <p>Choose a stage and find the assigned Pokemons!</p>
-      <p>See where you place on the scoreboard!</p>
+      <p>*See where you place on the scoreboard!*</p>
       <Levels
         props={props}
       />
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // include all React files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
